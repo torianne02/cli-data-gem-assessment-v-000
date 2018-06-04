2be42ec61f1cad8f1ab6ccf1a67182d29226b4ea
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-06-04 20:45:13 +0000

    automatically backed up by learn

---
+++ library/scraper.rb
@@ -0,0 +1,15 @@
+require 'nokogiri'
+require 'open-uri'
+require 'pry'
+
+class Scraper
+
+  class Books_library
+    def get_page
+      Nokogiri::HTML(open("https://www.goodreads.com/author/list/19823.Sylvia_Day?page=1&per_page=99999"))
+    end
+
+    def get_book_titles
+      book_title = self.get_page.css('a.bookTitle')
+      binding.pry
+    end
